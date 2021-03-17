79ef04f31467bc852eb4466897211e89900caa3a
MilkFather <31627231+MilkFather@users.noreply.github.com>
2021-03-17 15:05:35 +0800

    markdownit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import { MarkdownEditor } from './editor/editor.js'
 import { markdown } from './junk/markdown.js'
 
-let editor = new MarkdownEditor('editor-0', markdown);
+const urlParams = new URLSearchParams(window.location.search);
+let editor;
+if (urlParams.get('markdownit')) 
+    editor = new MarkdownEditor('editor-0', (src) => {
+        let md = window.markdownit();
+        return md.render(src);
+    });
+else 
+    editor = new MarkdownEditor('editor-0', markdown);
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('container').appendChild(editor.el);
